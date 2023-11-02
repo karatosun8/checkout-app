42bf167ba49e6cb4aaf692af5b1cd2308711ffe9
karatosun8 <sefa_karatosun@hotmail.com>
2023-11-02 11:03:42 +0300

    remove btn

---
+++ checkout.js
@@ -0,0 +1,22 @@
+const taxRate = 0.18;
+const shippingPrice = 25.99;
+const freeShippingPrice = 3000;
+
+//localStorage vs. sessionStorage
+window.addEventListener("load",()=>{
+    localStorage.setItem("taxRate",taxRate)
+    localStorage.setItem("shippingPrice",shippingPrice)
+    localStorage.setItem("freeShippingPrice",freeShippingPrice)
+})
+
+
+const navbarList = document.querySelector(".nav__list")
+const productList = document.querySelector("div.main__product-painel")
+
+//capturing
+navbarList.addEventListener("click",()=>{
+    if(e.target.className == "nav__list--btn"){
+        alert("remove btn click")
+
+    }
+})
